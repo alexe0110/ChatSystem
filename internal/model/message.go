448cd46b295151e94de36d36a9ba6257f202f0e2
Черкашин Алексей <alexe0110@yandex.ru>
2026-02-22 11:32:40 +0500

    models

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Message struct {
+	ID             uuid.UUID `json:"id"`
+	SenderID       uuid.UUID `json:"sender_id"`
+	ReceiverID     uuid.UUID `json:"receiver_id"`
+	MessageContent string    `json:"message_content"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
